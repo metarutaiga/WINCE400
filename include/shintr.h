b6c4ec8a4bffa2a65bd0b682ae664c02b413f04b
TAiGA <@>
2021-11-10 11:35:17 +0800

    WCE400

---
+++ include/shintr.h
@@ -0,0 +1,63 @@
+//
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//
+//
+// Use of this source code is subject to the terms of the Microsoft end-user
+// license agreement (EULA) under which you licensed this SOFTWARE PRODUCT.
+// If you did not accept the terms of the EULA, you are not authorized to use
+// this source code. For a copy of the EULA, please see the LICENSE.RTF on your
+// install media.
+//
+/***
+*shintr.h - definitions and declarations for SH intrinisc library
+*
+*Purpose:
+*       This file contains constant definitions and external subroutine
+*       declarations for the SH intrinisc subroutine library.
+*
+****/
+
+#ifndef _INC_SHINTR
+#define _INC_SHINTR
+
+#if defined(_SHX_) && ( defined(SH4) || defined(SH3) )
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+float * _Xform4dV(float *retval, float *vector, float *matrix);
+float * _Xform3dV(float *retval, float *vector, float *matrix);
+
+float * _XDXform4dV(float *src, float *dst);
+float * _XDXform3dV(float *src, float *dst);
+
+float  _Dot4dV(float *vector1, float *vector2);
+float  _Dot3dVW0(float *vector1, float *vector2);
+float  _Dot3dVW1(float *vector1, float *vector2);
+
+float * _Multiply4dM(float *retval, float *m4x4m1, float *m4x4m2);
+void _XDMultMatrix(float *matrix);
+
+float * _LoadMatrix(float *matrix);
+float * _SaveMatrix(float *matrix);
+
+float fabsf(float input);
+
+float sqrtf(float input);
+
+float __fmac(float i, float m, float n);
+
+void __prefetch(void *);
+void __movca(unsigned long value, unsigned long *addr);
+
+float _Convolve(int nelement, float *pstart, float *pend, 
+                float *pdata, float *pfilter);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //defined(_SHX_) && ( defined(SH4) || defined(SH3) )
+
+#endif //_INC_SHINTR
